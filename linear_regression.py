150192ca36d2e9583f97be5d5de344e295d3ff7c
William Kvaale <wgkvaale@gmail.com>
2019-09-15 00:50:02 +0200

    Implement 3D-plot and clean up

--- linear_regression.py
+++ linear_regression.py
@@ -3,6 +3,7 @@ from os import path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def h(x, weights):
@@ -31,8 +32,8 @@ def meanSquaredError(y_pred, y_actual):
     return model_error
 
 
-def train(filePath, xs='x1'):
-    X_train, y_train, df_train = preprocessData(filePath, [xs])
+def train(filePath, xs=['x1']):
+    X_train, y_train, df_train = preprocessData(filePath, xs)
 
     weights = calculateWeights(X_train, y_train)
 
@@ -42,8 +43,8 @@ def train(filePath, xs='x1'):
     return X_train, y_train, pred_ys_train, weights
 
 
-def test(filePath, weights, xs='x1'):
-    X_test, y_test, df_test = preprocessData(filePath, [xs])
+def test(filePath, weights, xs=['x1']):
+    X_test, y_test, df_test = preprocessData(filePath, xs)
 
     pred_ys_test = h(X_test, weights)
     pred_ys_test = pred_ys_test.rename(columns={0: 'y'})
@@ -52,14 +53,30 @@ def test(filePath, weights, xs='x1'):
 
 
 
-def plotting(X, y, pred_y, title, acc):
-    X = X.drop(['x0'], axis=1)
-    plt.plot(X, y, 'o', label='Original data', markersize=10)
-    plt.plot(X, pred_y, 'r', label='Fitted line')
-    plt.title(title)
-    plt.suptitle(f"Loss: {acc*100}%", va='top')
-    plt.legend()
-    plt.show()
+def plotting(X, y, pred_y, title, acc, threeDim=False):
+    if not threeDim:
+        X = X.drop(['x0'], axis=1)
+        plt.plot(X, y, 'o', label='Original data', markersize=10)
+        plt.plot(X, pred_y, 'r', label='Fitted line')
+        plt.title(title)
+        plt.suptitle(f"Loss: {acc*100}%", va='top')
+        plt.legend()
+        plt.show()
+    else:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        datapoints = ax.scatter(X['x1'], X['x2'], y, marker='v')
+        predictions = ax.scatter(X['x1'], X['x2'], pred_y, marker='^')
+
+        ax.legend([datapoints, predictions], ['datapoints', 'predictions'], numpoints=1)
+
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+        ax.set_zlabel('y')
+
+        plt.show()
+
 
 
 def run(d=1):
@@ -78,7 +95,7 @@ def run(d=1):
         print(f"bias:\t{weights_1d[0][0]}\nslope:\t{weights_1d[1][0]}")
 
         trained_model_error_1d = round(meanSquaredError(pred_ys_train_1d, y_train_1d), 4)
-        print(f"(Trained 1D-model error is:\t {trained_model_error_1d}"
+        print(f"Trained 1D-model error is: {trained_model_error_1d}"
               f"\n ~ {trained_model_error_1d * 100}%")
 
         plotting(X_train_1d, y_train_1d, pred_ys_train_1d, "Trained 1D", trained_model_error_1d)
@@ -87,7 +104,7 @@ def run(d=1):
         X_test_1d, y_test_1d, pred_ys_test_1d = test(test_1d, weights_1d)
 
         tested_model_error_1d = round(meanSquaredError(pred_ys_test_1d, y_test_1d), 4)
-        print(f"(Tested 1D-model error is:\t {tested_model_error_1d}"
+        print(f"Tested 1D-model error is: {tested_model_error_1d}"
               f"\n ~ {tested_model_error_1d * 100}%")
 
         plotting(X_test_1d, y_test_1d, pred_ys_test_1d, "Tested 1D", tested_model_error_1d)
@@ -96,29 +113,29 @@ def run(d=1):
     # 2D
     elif d == 2:
         # TRAINING
-        X_train_2d, y_train_2d, pred_ys_train_2d, weights_2d = train(train_2d, xs="'x1', 'x2'")
+        X_train_2d, y_train_2d, pred_ys_train_2d, weights_2d = train(train_2d, xs=['x1', 'x2'])
         print(f"w0 = bias:\t{weights_2d[0][0]}\nw1:\t\t{weights_2d[1][0]}\nw2:\t\t{weights_2d[2][0]}")
 
         trained_model_error_2d = round(meanSquaredError(pred_ys_train_2d, y_train_2d), 4)
-        print(f"(Trained 2D-model error is:\t {trained_model_error_2d}\n ~ {trained_model_error_2d * 100}%")
+        print(f"Trained 2D-model error is: {trained_model_error_2d}\n ~ {trained_model_error_2d * 100}%")
 
-        plotting(X_train_2d, y_train_2d, pred_ys_train_2d, "Trained 2D", trained_model_error_2d)
+        plotting(X_train_2d, y_train_2d, pred_ys_train_2d, "Trained 2D", trained_model_error_2d, threeDim=1)
 
         # TESTING
-        X_test_2d, y_test_2d, pred_ys_test_2d = test(test_2d, weights_2d, xs="'x1', 'x2'")
+        X_test_2d, y_test_2d, pred_ys_test_2d = test(test_2d, weights_2d, xs=['x1', 'x2'])
 
         tested_model_error_2d = round(meanSquaredError(pred_ys_test_2d, y_test_2d), 4)
-        print(f"(Tested 2D-model error is:\t {tested_model_error_2d}"
+        print(f"Tested 2D-model error is: {tested_model_error_2d}"
               f"\n ~ {tested_model_error_2d * 100}%")
 
-        plotting(X_test_2d, y_test_2d, pred_ys_test_2d, "Tested 2D", tested_model_error_2d)
-
+        plotting(X_test_2d, y_test_2d, pred_ys_test_2d, "Tested 2D", tested_model_error_2d, threeDim=1)
 
     else:
-        return "Choose run(1) or run(2)"
+        print("'1' --> 1D\n'2' --> 2D")
 
-run()
+
 try:
     run(int(argv[1]))
-except:
-    print("Use '1' or '2' to tell sys.argv what dataset to run")
+except Exception as e:
+    print("Use '1' or '2' to tell sys.argv which dataset to run")
+    print(f"Exception!\n{e}")
