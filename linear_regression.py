0c640f296ae7f36172a94ba8acded38bf00ab420
William Kvaale <wgkvaale@gmail.com>
2019-09-14 23:47:56 +0200

    Implement linear regression for 1D and 2D

---
+++ linear_regression.py
@@ -0,0 +1,117 @@
+from os import path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def h(x, weights):
+    return x.dot(weights)
+
+
+def calculateWeights(X, y):
+    return np.dot(np.linalg.pinv(np.dot(X.T, X)), np.dot(X.T, y))
+
+
+def preprocessData(filePath, xs):
+    df = pd.read_csv(filePath)
+    X = df[xs]
+    X.insert(0, 'x0', 1, True) # Add new dimension because of merging bias into weight vector --> w0 = bias
+    y = df[['y']]
+    return X, y, df
+
+
+def meanSquaredError(y_pred, y_actual):
+    errorFrame = pd.DataFrame(columns=['y_pred', 'y_actual', 'mse'])
+    errorFrame['y_pred'] = y_pred['y']
+    errorFrame['y_actual'] = y_actual['y']
+    errorFrame['mse'] = (y_pred['y'] - y_actual['y'])**2
+
+    model_error = sum(errorFrame['mse'])/len(errorFrame['mse'])
+    return model_error
+
+
+def train(filePath, xs='x1'):
+    X_train, y_train, df_train = preprocessData(filePath, [xs])
+
+    weights = calculateWeights(X_train, y_train)
+
+    pred_ys_train = h(X_train, weights)
+    pred_ys_train = pred_ys_train.rename(columns={0: 'y'})
+
+    return X_train, y_train, pred_ys_train, weights
+
+
+def test(filePath, weights, xs='x1'):
+    X_test, y_test, df_test = preprocessData(filePath, [xs])
+
+    pred_ys_test = h(X_test, weights)
+    pred_ys_test = pred_ys_test.rename(columns={0: 'y'})
+
+    return X_test, y_test, pred_ys_test
+
+
+
+def plotting(X, y, pred_y):
+    X = X.drop(['x0'], axis=1)
+    plt.plot(X, y, 'o', label='Original data', markersize=10)
+    plt.plot(X, pred_y, 'r', label='Fitted line')
+    plt.legend()
+    plt.show()
+
+
+def run(d=1):
+    # DATASET
+    train_1d = path.abspath("dataset/regression/train_1d_reg_data.csv")
+    train_2d = path.abspath("dataset/regression/train_2d_reg_data.csv")
+
+    test_1d = path.abspath("dataset/regression/test_1d_reg_data.csv")
+    test_2d = path.abspath("dataset/regression/test_2d_reg_data.csv")
+
+    # 1D
+    if d == 1:
+        # TRAINING
+        X_train_1d, y_train_1d, pred_ys_train_1d, weights_1d = train(train_1d)
+
+        print(f"bias:\t{weights_1d[0][0]}\nslope:\t{weights_1d[1][0]}")
+
+        trained_model_error_1d = round(meanSquaredError(pred_ys_train_1d, y_train_1d), 4)
+        print(f"(Trained 1D-model error is:\t {trained_model_error_1d}"
+              f"\n ~ {trained_model_error_1d * 100}%")
+
+        plotting(X_train_1d, y_train_1d, pred_ys_train_1d)
+
+        # TESTING
+        X_test_1d, y_test_1d, pred_ys_test_1d = test(test_1d, weights_1d)
+
+        tested_model_error_1d = round(meanSquaredError(pred_ys_test_1d, y_test_1d), 4)
+        print(f"(Tested 1D-model error is:\t {tested_model_error_1d}"
+              f"\n ~ {tested_model_error_1d * 100}%")
+
+        plotting(X_test_1d, y_test_1d, pred_ys_test_1d)
+
+
+    # 2D
+    elif d == 2:
+        # TRAINING
+        X_train_2d, y_train_2d, pred_ys_train_2d, weights_2d = train(train_2d, xs="'x1', 'x2'")
+        print(f"w0 = bias:\t{weights_2d[0][0]}\nw1:\t\t{weights_2d[1][0]}\nw2:\t\t{weights_2d[2][0]}")
+
+        trained_model_error_2d = round(meanSquaredError(pred_ys_train_2d, y_train_2d), 4)
+        print(f"(Trained 2D-model error is:\t {trained_model_error_2d}\n ~ {trained_model_error_2d * 100}%")
+
+        plotting(X_train_2d, y_train_2d, pred_ys_train_2d)
+
+        # TESTING
+        X_test_2d, y_test_2d, pred_ys_test_2d = test(test_2d, weights_2d, xs="'x1', 'x2'")
+
+        tested_model_error_2d = round(meanSquaredError(pred_ys_test_2d, y_test_2d), 4)
+        print(f"(Tested 2D-model error is:\t {tested_model_error_2d}"
+              f"\n ~ {tested_model_error_2d * 100}%")
+
+        plotting(X_test_2d, y_test_2d, pred_ys_test_2d)
+
+
+    else:
+        return "Choose run(1) or run(2)"
+
+run()
