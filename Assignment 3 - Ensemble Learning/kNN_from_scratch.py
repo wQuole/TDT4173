0966481fde694c31520dae5e2b47d3dea7d60a28
William Kvaale <wgkvaale@gmail.com>
2019-11-16 23:34:54 +0100

    Rename Assignment 3 correctly and add gitignore

--- Assignment 3 - Ensemble Learning/kNN_from_scratch.py
+++ Assignment 3 - Ensemble Learning/kNN_from_scratch.py
@@ -1,19 +1,12 @@
 import numpy as np
+import pandas as pd
 from os import path
-from math import sqrt, pow
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def euclidean_distance(v, u, length):
-    distance = 0
-    for x in range(length):
-        distance += sqrt(pow((v[x] - u[x]), 2))
-    return distance
-
-
-def load_data(filePath):
+def np_load_data(filePath):
     data = np.genfromtxt(filePath, skip_header=True, delimiter=',')
     X = data[:, :-1]
     y = data[:, 3]
@@ -21,12 +14,19 @@ def load_data(filePath):
     return X_train, X_test, y_train, y_test
 
 
-def get_neighbor(X_train, X_instance, k):
-    return None
+def load_data(filePath):
+    dataset = pd.read_csv(filePath, header=None)
+    X_train = dataset[dataset.columns[:-1]]
+    y = dataset[dataset.columns[-1]]
+    return X_train, y, dataset
 
 
-def predict(X_test):
-    return None
+def euclidean_distance(candidate, data):
+    distance = 0
+    for i in range(len(candidate)):
+        print(f"{candidate} --> {i}")
+        distance += np.square(candidate[i] - data[i])
+    return np.sqrt(distance)
 
 
 def evaluate(y_test, y_pred):
@@ -42,6 +42,11 @@ def pretty_print(data, head=False):
         data = data[:10]
     for i in range(len(data)):
         print(f"{i}:\t {data[i]}")
+
+
+def plurality_vote(all_neighbors):
+    # returns the neighbors with most occurrences
+    return np.argmax(np.bincount(all_neighbors))
 
 
 def plotting(X, y, title, acc=1, threeDim=False):
@@ -67,11 +72,24 @@ def plotting(X, y, title, acc=1, threeDim=False):
 
         plt.show()
 
+def kNN(X_train, test_Instance, k=10):
+    distances = []
+    voting_dict = {}
+    for x in X_train:
+        distances.append(euclidean_distance(test_Instance, X_train.iloc[x]))
+    sorted = np.argsort(distances)[:10]
+
 
 if __name__ == '__main__':
     knn_regression_data = path.abspath("dataset/knn_regression.csv")
-    X_train, X_test, y_train, y_test = load_data(knn_regression_data)
-    pretty_print(X_train, 1)
-    plotting(X_train, y_train, "Original data", threeDim=True)
-    print(f"Number of elements:\t{X_train.size}")
-    print(f"Shape of the data:\t{np.shape(X_train)}")
+    # X_train, X_test, y_train, y_test = np_load_data(knn_regression_data)
+    X_train, y_train, dataset = load_data(knn_regression_data)
+    print(type(X_train))
+    # plotting(X_train, y_train, "Original data", threeDim=True)
+    # print(f"Number of elements:\t{X_train.size}")
+    # print(f"Shape of the data:\t{np.shape(X_train)}")
+    # print(dataset.head())
+    kNN(X_train, pd.DataFrame(X_train.iloc[124]), k=10)
+
+
+
